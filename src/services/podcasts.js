60b8539a282d219c9933ede56300268e0fa805ec
Miguel Alonso <malbonm07@gmail.com>
2022-04-24 14:31:36 -0500

    Changed cors proxy for services

--- src/services/podcasts.js
+++ src/services/podcasts.js
@@ -2,14 +2,18 @@ import http, {config} from '../utils/http';
 import api from '../constants/api';
 const { parse } = require('rss-to-json');
 
+// const corsProxy = 'https://cors-anywhere.herokuapp.com/'
+const corsProxy = 'https://thingproxy.freeboard.io/fetch/'
+
+
 export function getPodcasts() {
-    return http.get(`https://cors-anywhere.herokuapp.com/${api.PODCASTS}`)
+    return http.get(`${corsProxy}${api.PODCASTS}`)
 }
 
 export function getPodcast(params) {
-    return http.get(`https://cors-anywhere.herokuapp.com/${api.PODCAST}`, {params})
+    return http.get(`${corsProxy}${api.PODCAST}`, {params})
 }
 
 export function getPodcastEpisodes(params) {
-    return parse(`https://cors-anywhere.herokuapp.com/${params}`, config)
+    return parse(`${corsProxy}${params}`, config)
 }
