b5e816a2b48832e1189a2e9b764d03ba469d40aa
Miguel Alonso <malbonm07@gmail.com>
2022-04-25 08:19:11 -0500

    put correct proxy url

--- src/constants/proxies.js
+++ src/constants/proxies.js
@@ -1,7 +1,7 @@
 const PROXIES = Object.freeze({
     CORS_ANYWHERE: 'https://cors-anywhere.herokuapp.com/',
     THINGPROXY: 'https://thingproxy.freeboard.io/fetch/',
-    CUSTOM_PROXY: 'http://localhost:8000/'
+    CUSTOM_PROXY: 'https://node-podcaster.herokuapp.com/'
     // CUSTOM_PROXY: 'https://node-podcaster.herokuapp.com/'
 })
 
